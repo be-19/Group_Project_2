763d3276bf574d3e5a28cb390ceaff870879716c
Andrian Ramadan <ramadanandrian89@gmail.com>
2022-11-20 09:05:02 +0700

    adding authentication and authorization to konsultasi

--- routes/konsultasi.router.js
+++ routes/konsultasi.router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 
 const {
   getAllKonsultasi,
@@ -9,10 +10,27 @@ const {
   deleteKonsultasi,
 } = require("../controllers/konsultasi.controller");
 
-router.get("/", getAllKonsultasi);
-router.get("/:konsultasiId", getKonsultasiById);
-router.post("/", addKonsultasi);
-router.patch("/:konsultasiId", updateKonsultasi);
-router.delete("/:konsultasiId", deleteKonsultasi);
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+  next();
+});
+
+router.get("/", [auth.verifyToken, auth.isNakes], getAllKonsultasi);
+router.get(
+  "/:konsultasiId",
+  [auth.verifyToken, auth.isNakes],
+  getKonsultasiById
+);
+router.post("/", [auth.verifyToken, auth.isNakes], addKonsultasi);
+router.patch(
+  "/:konsultasiId",
+  [auth.verifyToken, auth.isNakes],
+  updateKonsultasi
+);
+router.delete(
+  "/:konsultasiId",
+  [auth.verifyToken, auth.isNakes],
+  deleteKonsultasi
+);
 
 module.exports = router;

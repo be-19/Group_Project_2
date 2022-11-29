5ee6daaebc4d10d51c718e8a9a75096b957daf09
Andrian Ramadan <ramadanandrian89@gmail.com>
2022-11-29 15:57:05 +0700

    add getKonsultasibyPatientID

--- controllers/pasien.controller.js
+++ controllers/pasien.controller.js
@@ -97,11 +97,12 @@ module.exports = {
         { _id: req.params.id },
         { $set: req.body }
       );
-      res
-        .status(200)
-        .json({ message: "berhasil update data", data: updatedPasien });
+      res.status(200).json({
+        message: "Successfully update patient data",
+        data: updatedPasien,
+      });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: "Internal server error", data: error });
     }
   },
 };
